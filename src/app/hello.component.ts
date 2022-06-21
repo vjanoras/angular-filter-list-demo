a11310221fbfebc8b2ad300129c60de8541170d8
vjanoras <val.janoras@outlook.com>
2022-06-21 13:32:34 -0400

    2 files modified, 3 added, 1 deleted

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,82 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
+
+//https://stackblitz.com/edit/display-image-from-api?file=src%2Fapp%2Fconfig.service.ts
+//https://stackoverflow.com/questions/55057774/how-to-get-image-and-show-in-my-page-angular-6
 
 @Component({
   selector: 'hello',
-  template: `Hello {{name}}!`
+  //template: `Hello {{name}}!`,
+  templateUrl: './hello.html',
 })
-export class HelloComponent {
+export class HelloComponent implements OnInit {
   @Input()
-  public name: string = "World";
+  public name: string = 'World';
+
+  private httpClient: HttpClient;
+  private _sanitizer: DomSanitizer;
+
+  image: any;
+  imageObject: any;
+  thumbnail: any;
+  thumbnail2: any;
+  imageTypeShow = false;
+
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {
+    this.httpClient = http;
+    this._sanitizer = sanitizer;
+  }
+
+  ngOnInit(): void {
+    // option 1: load from url (blob)
+    this.getImage().subscribe(
+      (data: any) => {
+        this.createImageFromBlob(data);
+      },
+      (error) => {}
+    );
+
+    // option 2: load from config (base64)
+    this.getImage2().subscribe(
+      (data: any) => {
+        let objectURL = 'data:image/jpeg;base64,' + data.image;
+        this.thumbnail2 = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+        console.log('option2', this.thumbnail2);
+      },
+      (error) => {}
+    );
+  }
+
+  getImage() {
+    return this.httpClient.get('/assets/testimage.jpeg', {
+      responseType: 'blob',
+    });
+  }
+
+  getImage2() {
+    return this.httpClient.get('/assets/config.json');
+  }
+
+  createImageFromBlob(image: Blob) {
+    let reader = new FileReader();
+    reader.addEventListener(
+      'load',
+      () => {
+        let img = reader.result;
+        // console.log(img.toString());
+
+        this.thumbnail = this._sanitizer.bypassSecurityTrustUrl(img.toString());
+        console.log('option1', this.thumbnail);
+        this.imageTypeShow = true;
+      },
+      false
+    );
+
+    if (image) {
+      if (image.type !== 'application/pdf') reader.readAsDataURL(image);
+    }
+  }
 }
--- src/app/search.component.ts
+++ src/app/search.component.ts
@@ -1,63 +1,66 @@
-import { 
-  Component 
-} from "@angular/core";
+import { Component } from '@angular/core';
 
-import { 
-  HttpClient 
-} from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 
 import {
-  IListColumn, ListColumn, ListColumnType, ListOptions
-} from "./widget/list.model";
+  IListColumn,
+  ListColumn,
+  ListColumnType,
+  ListOptions,
+} from './widget/list.model';
 
 @Component({
   selector: 'search',
-  templateUrl: './search.component.html'
+  templateUrl: './search.component.html',
 })
 export class SearchComponent {
-
   title = 'Dashboard Report List Widget';
   columns: IListColumn[] = [];
   options = new ListOptions();
 
   constructor(private http: HttpClient) {
-    
-    this.options.filterBy = [ "name", "type"];
-    this.options.sortBy = "name DESC";
-    this.options.serviceUrl = "../assets/test-data.json";
+    this.options.filterBy = ['name', 'type'];
+    this.options.sortBy = 'name DESC';
+    this.options.serviceUrl = '../assets/test-data.json';
 
-    this.columns.push(new ListColumn({ 
-        dataField: "name",           
-        displayText: "Name",
+    this.columns.push(
+      new ListColumn({
+        dataField: 'name',
+        displayText: 'Name',
         enableFiltering: true,
-        columnType: ListColumnType.Data
-    }));
+        columnType: ListColumnType.Data,
+      })
+    );
 
-    this.columns.push(new ListColumn({ 
-        dataField: "type",           
-        displayText: "Type",
+    this.columns.push(
+      new ListColumn({
+        dataField: 'type',
+        displayText: 'Type',
         enableFiltering: true,
-        columnType: ListColumnType.Data
-    }));
-    
-    this.columns.push(new ListColumn({ 
-        displayText: "Actions",
+        columnType: ListColumnType.Data,
+      })
+    );
+
+    this.columns.push(
+      new ListColumn({
+        displayText: 'Actions',
         enableFiltering: true,
         columnType: ListColumnType.Actions,
-        actionMenu: [ {
-          tooltip: "add dashboard to favorite",
-          actionType: "favorite",
-          destination: "",
-          iconCss: "fal fa-star"
-        },
-        {
-          tooltip: "view dashboard",
-          actionType: "view",
-          destination: "",
-          iconCss: "fal fa-eye"
-        }]
-      }));
-    }
-
-
+        actionMenu: [
+          {
+            tooltip: 'add dashboard to favorite',
+            actionType: 'favorite',
+            destination: '',
+            iconCss: 'fal fa-star',
+          },
+          {
+            tooltip: 'view dashboard',
+            actionType: 'view',
+            destination: '',
+            iconCss: 'fal fa-eye',
+          },
+        ],
+      })
+    );
+  }
 }
